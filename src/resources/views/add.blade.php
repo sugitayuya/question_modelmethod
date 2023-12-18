3824c0d968021908c3e36507ca930ac1207cec08
sugita yuya <yuya16578@gmail.com>
2023-12-19 06:40:43 +0900

    エラーメッセージの追加

--- src/resources/views/add.blade.php
+++ src/resources/views/add.blade.php
@@ -23,6 +23,13 @@
 @section('title', 'add.blade.php')
 
 @section('content')
+@if (count($errors) > 0)
+<ul>
+  @foreach ($errors->all() as $error)
+  <li>{{$error}}</li>
+  @endforeach
+</ul>
+@endif
 <form action="/add" method="post">
   <table>
   @csrf
--- src/routes/web.php
+++ src/routes/web.php
@@ -6,3 +6,6 @@ use App\Http\Controllers\AuthorController;
 Route::get('/find', [AuthorController::class, 'find']);
 Route::post('/find', [AuthorController::class, 'search']);
 Route::get('/author/{author}', [AuthorController::class, 'bind']);
+Route::get('/', [AuthorController::class, 'index']);
+Route::get('/add', [AuthorController::class, 'add']);
+Route::post('/add', [AuthorController::class, 'add']);
